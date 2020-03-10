98b21b246ac1333eca89ffc1a6bcb4da27ab368c
Clément Corbin <corbin.clement@gmail.com>
2020-03-10 17:06:04 +0100

    parametric

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import fitz
-print(fitz.__doc__)
-doc = fitz.open("./resume.pdf")
-links = doc.loadPage(0).getLinks()
-print(links)
-doc.loadPage(0).insertLink({'kind': 2, 'from': fitz.Rect(6.0, 350.0, 184.0, 366.0), 'uri': 'https://some-great-uri'})
-doc.save("out.pdf")
-doc.close();
+import sys
+if (len(sys.argv) != 4):
+  print("Usage: python "+sys.argv[0]+" source.pdf links_dump output.pdf")
+else:
+  doc = fitz.open(sys.argv[1])
+  linksToAdd = 	open(sys.argv[2],"r").read().split("\n")
+  for l in linksToAdd:
+    if (l != ""):
+      l = l.split("\t")
+      doc.loadPage(0).insertLink({"kind": 2, "from": fitz.Rect(*list(map(int,l[0].split(",")))), "uri": l[1]})
+  doc.save(sys.argv[3])
+  doc.close();
